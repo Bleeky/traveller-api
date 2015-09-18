afd32be17899f3e2084dd4239e63bee35b7293a6
Quentin <hausser.quentin@gmail.com>
2015-09-18 15:49:57 +0900

    PlaceService added with new set of restful routes. Updated Gitignore.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/emicklei/go-restful"
 	"log"
 	"net/http"
+	"traveller-api/placeservice"
 	"traveller-api/travelservice"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	restful.DefaultResponseContentType(restful.MIME_JSON)
 	restful.DefaultRequestContentType(restful.MIME_JSON)
 	restful.Add(travelservice.New())
+	restful.Add(placeservice.New())
 	server := &http.Server{Addr: ":8080"}
 	log.Fatal(server.ListenAndServe())
 }
---
+++ placeservice/placeservice.go
@@ -0,0 +1,80 @@
+package placeservice
+
+import (
+	"github.com/emicklei/go-restful"
+	"traveller-api/utils"
+)
+
+type PlaceResource struct {
+	Id     int    `db:"id" json:"id"`
+	Coords string `db:"coords" json:"coords"`
+}
+
+func New() *restful.WebService {
+	service := new(restful.WebService)
+	service.Path("/places").
+		Consumes(restful.MIME_JSON).
+		Produces(restful.MIME_JSON)
+	service.Route(service.GET("").To(GetPlaces))
+	service.Route(service.GET("/{place-id}").To(FindPlace))
+	service.Route(service.POST("").To(CreatePlace))
+	service.Route(service.PUT("/{place-id}").To(UpdatePlace))
+	service.Route(service.DELETE("/{place-id}").To(DeletePlace))
+	return service
+}
+func GetPlaces(request *restful.Request, response *restful.Response) {
+	session := utils.NewSession()
+	rows, err := session.DB.Query("SELECT id, coords FROM place")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	var results []PlaceResource
+	var place PlaceResource
+	for rows.Next() {
+		err := rows.Scan(&place.Id, &place.Coords)
+		if err != nil {
+			panic(err)
+		}
+		results = append(results, place)
+	}
+	session.Close()
+	response.WriteAsJson(results)
+}
+func UpdatePlace(request *restful.Request, response *restful.Response) {
+
+}
+func DeletePlace(request *restful.Request, response *restful.Response) {
+
+}
+func FindPlace(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("travel-id")
+	session := utils.NewSession()
+	rows, err := session.DB.Query("SELECT id, coords FROM travel WHERE id = " + id)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	var place PlaceResource
+	for rows.Next() {
+		err := rows.Scan(&place.Id, &place.Coords)
+		if err != nil {
+			panic(err)
+		}
+	}
+	session.Close()
+	response.WriteAsJson(place)
+}
+func CreatePlace(request *restful.Request, response *restful.Response) {
+	// travel := new(Travel)
+	// err := request.ReadEntity(&travel)
+	// session := utils.NewSession()
+	// c := session.DB("traveller").C("travels")
+	// c.Insert(&travel)
+	// session.Close()
+	// if err == nil {
+	// 	response.WriteEntity(travel)
+	// } else {
+	// 	response.WriteError(http.StatusInternalServerError, err)
+	// }
+}
